819d5151e87a14791611ca8508b5fb223bcc874b
Leigh Caplan <lcaplan@onehub.com>
2012-03-20 17:17:20 -0700

    Rudimentary observer

--- helpbot-demo.js
+++ helpbot-demo.js
@@ -1,2 +1,26 @@
 // helpbot-demo.js
 // For testing out the Helpbot API
+
+// Register a simple callback
+
+$( function () {
+  Helpbot.register({
+    when: function () {
+      return $('h1').length;
+    },
+
+    action: function () {
+      console.log("there is an h1 on the page!");
+    }
+  });
+
+  Helpbot.register({
+    when: function () {
+      return $('p').length;
+    },
+
+    action: function () {
+      console.log('definitely a p tag, too');
+    }
+  });
+});
